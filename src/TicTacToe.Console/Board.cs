daffca97ece248076a202d8c8b8299f6de63516e
m Warner <mwarner1@jeffersonstate.edu>
2023-02-03 21:07:10 -0600

    added Board function to check if coordinate has been used

--- src/TicTacToe.Console/Board.cs
+++ src/TicTacToe.Console/Board.cs
@@ -19,6 +19,48 @@ namespace TicTacToe
                 board[i] = ' ';
             }
         }
+        public bool checkIfCellAvailable(int coordinate,char flag)
+        {
+            if (board[coordinate] != ' ')
+            {
+                return false;
+            }
+            switch (coordinate)
+            {
+                case 1:
+                    board[coordinate-1] = flag;
+                    break;
+                case 2:
+                    board[coordinate-1] = flag;
+                    break;
+                case 3:
+                    board[coordinate-1] = flag;
+                    break;
+                case 4:
+                    board[coordinate-1] = flag;
+                    break;
+                case 5:
+                    board[coordinate-1] = flag;
+                    break;
+                case 6:
+                    board[coordinate - 1] = flag;
+                    break;
+                case 7:
+                    board[coordinate - 1] = flag;
+                    break;
+                case 8:
+                    board[coordinate - 1] = flag;
+                    break;
+                case 9:
+                    board[coordinate - 1] = flag;
+                    break;
+                default:
+                    return false;
+            }
+            return true;   
+
+        } 
+
         public void printBoard()
         {
 
@@ -29,6 +71,7 @@ namespace TicTacToe
             WriteLine("    \t\t\t\t {0} | {1} | {2} ", board[6], board[7], board[8]);
         }
 
+      
 
     }
 }
